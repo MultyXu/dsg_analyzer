944712f330995eb4c21c014b1425f864d416534d
Multy <multyxu@gmail.com>
2025-09-05 10:27:26 -0400

    publish the analyzed statistics to ROS

--- dsg_analyzer/dsg_analyzer.py
+++ dsg_analyzer/dsg_analyzer.py
@@ -4,6 +4,7 @@ import threading
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+
 from hydra_ros import DsgSubscriber
 import spark_dsg as dsg
 
@@ -15,13 +16,29 @@ class DsgAnalyzer(Node):
         self.dsg_lock = threading.Lock()
         self.last_dsg_time = None
         DsgSubscriber(self, "/hamilton/hydra/backend/dsg", self.dsg_callback)
+        
+        self.msg_string = ""
+        # publish data for the rviz panel
+        self.publisher_ = self.create_publisher(String, '/dsg_statistics', 10)
+        timer_period = 1.0  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def print_dsg_statistics(self, time, G):
-        self.get_logger().info(f"DSG Statistics at time {time:.2f} seconds:")
-        self.get_logger().info(f"Number of objects: {G.get_layer(dsg.DsgLayers.OBJECTS).num_nodes()}")
-        self.get_logger().info(f"Number of places: {G.get_layer(dsg.DsgLayers.PLACES).num_nodes()}")
-        self.get_logger().info(f"Number of rooms: {G.get_layer(dsg.DsgLayers.ROOMS).num_nodes()}")
-        
+        self.msg_string = f"DSG Statistics at time {time:.2f} seconds:\n"
+        self.msg_string += f"Number of objects: {G.get_layer(dsg.DsgLayers.OBJECTS).num_nodes()}\n"
+        # self.msg_string += f"Number of places: {G.get_layer(dsg.DsgLayers.PLACES).num_nodes()}\n"
+        self.msg_string += f"Number of places: {G.get_layer(dsg.DsgLayers.MESH_PLACES).num_nodes()}\n"
+        self.msg_string += f"Number of rooms: {G.get_layer(dsg.DsgLayers.ROOMS).num_nodes()}\n"
+
+        # publish in info logger
+        self.get_logger().info(self.msg_string)
+    
+    def timer_callback(self):
+        # create a string message and publish it
+        msg = String()
+        msg.data = self.msg_string
+        self.publisher_.publish(msg)
+
     def dsg_callback(self, header, dsg):
         with self.dsg_lock:
             self.dsg = dsg
